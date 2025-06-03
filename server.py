d05804171eee5079612eb6ce817966f6a76eca27
sonxii <alicelinew@gmail.com>
2025-06-03 19:29:58 +0800

    Update server.py

--- server.py
+++ server.py
@@ -157,6 +157,4 @@ def get_stage(count):
         return 'chaos'
 
 if __name__ == '__main__':
-    import os
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=5001, threaded=True)
